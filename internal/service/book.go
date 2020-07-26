f8fd13f5f9915dabff488c3819dcf12ad5e3da52
Marcos Filho <m.filhow>
2020-07-25 23:01:27 -0300

    updating the findall signature.

--- internal/service/book.go
+++ internal/service/book.go
@@ -6,7 +6,7 @@ import (
 
 type BookService interface {
 	Save(book book.Book) (*book.Book, error)
-	FindAll() error
+	FindAll() (*[]book.Book, error)
 	FindById(id uint64) (*book.Book, error)
 	Delete(id uint64) error
 	Update(book book.Book) error
@@ -23,8 +23,8 @@ func (bookServiceImpl) Save(book book.Book) (*book.Book, error) {
 	return nil, nil
 }
 
-func (bookServiceImpl) FindAll() error {
-	return nil
+func (bookServiceImpl) FindAll() (*[]book.Book, error) {
+	return nil, nil
 }
 
 func (bookServiceImpl) FindById(id uint64) (*book.Book, error) {
