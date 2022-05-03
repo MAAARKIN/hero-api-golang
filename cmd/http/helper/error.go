d336b09c35e310d601d55ecd4c991049175d681b
Marcos Filho <marcos.filho@unico.io>
2022-05-02 21:31:17 -0300

    Update error.go

--- cmd/http/helper/error.go
+++ cmd/http/helper/error.go
@@ -12,6 +12,6 @@ func DealWith(err error) HttpError {
 
 	return HttpError{
 		Status:      http.StatusInternalServerError,
-		Description: "Internal error, please report to unico Team",
+		Description: "Internal error, please report to admin",
 	}
 }
